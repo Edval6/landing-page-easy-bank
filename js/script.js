27898fbe80384523693de2bbbe22753d8822855f
Edival Tembe <edvaltembe@gmail.com>
2023-01-18 00:37:42 +0200

    responsividade do menu mobile

--- js/script.js
+++ js/script.js
@@ -3,4 +3,23 @@ var menu = document.querySelector("#menu-icon");
 menu.onclick = function(){
     var menuMobile = document.querySelector("#menu-mobile");
     menuMobile.style.display = "block";
+
+    menu.style.display = "none";
+
+    var close = document.querySelector("#close-icon");
+
+    close.style.display = "block";
+
+
+}
+
+var close = document.querySelector("#close-icon");
+
+close.onclick = function(){
+    var closeMobile = document.querySelector("#menu-mobile");
+    closeMobile.style.display = "none";
+
+    menu.style.display = "block";
+    close.style.display = "none";
+
 }
